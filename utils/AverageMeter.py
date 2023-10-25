b307284b486f774f4d4f83955325bcb8855e2e12
flixmk <krause.felix.fk@gmail.com>
2023-10-26 00:16:23 +0200

    added synthesis

---
+++ utils/AverageMeter.py
@@ -0,0 +1,12 @@
+class AverageMeter:
+    def __init__(self, name=None):
+        self.name = name
+        self.reset()
+
+    def reset(self):
+        self.sum = self.count = self.avg = 0
+
+    def update(self, val, n=1):
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
